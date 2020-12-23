e096bc1cfa5818a205e30a270ca104dd2b770b8c
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2020-12-23 14:06:33 +0600

    App Design #30 : Chef's Registration Page

--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/MainActivity.java
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landingpage);
+        setContentView(R.layout.activity_chef_registration);
     }
 }
