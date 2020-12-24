595ca3fe6c550afbf93e68be1922df59bbf50af5
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2020-12-24 18:10:47 +0600

    Midterm Task 2: SMS app
    Designing the main activity

---
+++ Assignments/Theory/Midterm/Task2 App/SmsApp/app/src/main/java/com/jubair/nsu/cse486/sec1/midterm/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.jubair.nsu.cse486.sec1.midterm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
