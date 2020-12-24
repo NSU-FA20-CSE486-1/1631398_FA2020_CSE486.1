46668e87a6a6d6579d89b3de6741f65024b8ed2f
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2020-12-24 18:56:06 +0600

    Midterm Task 2 : SMS App
    Designing Second Activity

--- Assignments/Theory/Midterm/Task2 App/SmsApp/app/src/main/java/com/jubair/nsu/cse486/sec1/midterm/MainActivity.java
+++ Assignments/Theory/Midterm/Task2 App/SmsApp/app/src/main/java/com/jubair/nsu/cse486/sec1/midterm/MainActivity.java
@@ -2,13 +2,26 @@ package com.jubair.nsu.cse486.sec1.midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.jubair.nsu.cse486.sec1.midterm.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void sendMessage(View view) {
+     //Write Function
+    }
+
+
 }
