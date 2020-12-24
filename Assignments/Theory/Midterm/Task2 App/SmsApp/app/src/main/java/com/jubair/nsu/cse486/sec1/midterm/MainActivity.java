f159287f8f20f3a4e834a2b065f598037a153b60
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2020-12-25 00:31:49 +0600

    Midterm Task 2 SMS App : Passing Encrypted Message
    Passing Encrypted Message from First to Second Activity

--- Assignments/Theory/Midterm/Task2 App/SmsApp/app/src/main/java/com/jubair/nsu/cse486/sec1/midterm/MainActivity.java
+++ Assignments/Theory/Midterm/Task2 App/SmsApp/app/src/main/java/com/jubair/nsu/cse486/sec1/midterm/MainActivity.java
@@ -9,21 +9,38 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.jubair.nsu.cse486.sec1.midterm.MESSAGE";
-
+    EditText writeMsg;
+    EditText key;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        writeMsg =  findViewById(R.id.editText_WriteMsg);
+        key =  findViewById(R.id.editText_WriteKey);
+    }
+
+    public String encrypt(String plainText, String secretKey) {
+        StringBuffer encryptedString = new StringBuffer();
+        int encryptedInt;
+        for (int i = 0; i < plainText.length(); i++) {
+            int plainTextInt = (int) (plainText.charAt(i) - 'A');
+            int secretKeyInt = 3;
+            encryptedInt = (plainTextInt + secretKeyInt) % 26;
+            encryptedString.append((char) ((encryptedInt) + (int) 'A'));
+        }
+        return encryptedString.toString();
     }
 
 
     public void sendMessage(View view) {
+        String plaintext = writeMsg.getText().toString();
+        String secretkey = key.getText().toString();
+        String encryptedText = encrypt( plaintext , secretkey);
+
         Intent i = new Intent(this, SecondActivity.class);
-        EditText writeMsg = (EditText) findViewById(R.id.editText_WriteMsg);
-        String message = writeMsg.getText().toString();
-        i.putExtra(EXTRA_MESSAGE, message);
+        i.putExtra(EXTRA_MESSAGE, encryptedText);
         startActivity(i);
     }
 
