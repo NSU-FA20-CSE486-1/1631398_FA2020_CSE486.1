7fb334bdf2624cdf1bc77cfd5401c92c641bc9c8
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2020-12-30 01:17:30 +0600

    App Design #34 : Chef's Homepage

---
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Chef/Chef_Homepage.java
@@ -0,0 +1,19 @@
+package com.jubair.nsu.cse486.sec1.foodies.Chef;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.os.Bundle;
+import com.jubair.nsu.cse486.sec1.foodies.R;
+
+public class Chef_Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_homepage);
+    }
+
+
+}
---
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Chef/Home.java
@@ -0,0 +1,12 @@
+package com.jubair.nsu.cse486.sec1.foodies.Chef;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jubair.nsu.cse486.sec1.foodies.R;
+
+public class Home extends AppCompatActivity {
+
+
+}
