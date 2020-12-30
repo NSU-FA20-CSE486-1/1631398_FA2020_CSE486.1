2004cd5815db3fca046431684139aa27442d06be
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2020-12-31 01:25:10 +0600

    App Design #35 : Chef's Kitchen Page
    Only the layout is designed. Other works are related to database like fetching data and show.

---
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Chef/Chef_Kitchen.java
@@ -0,0 +1,16 @@
+package com.jubair.nsu.cse486.sec1.foodies.Chef;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jubair.nsu.cse486.sec1.foodies.R;
+
+public class Chef_Kitchen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_kitchen);
+    }
+}
