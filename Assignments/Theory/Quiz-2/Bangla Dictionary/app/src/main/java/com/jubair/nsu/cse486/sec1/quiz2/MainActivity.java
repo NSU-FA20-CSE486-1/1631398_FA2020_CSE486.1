96ef716b5aea6a3c100b52729311975b26ae89b9
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2021-01-06 12:15:50 +0600

    Quiz-2 Created RecycleView and Adapter

--- Assignments/Theory/Quiz-2/Bangla Dictionary/app/src/main/java/com/jubair/nsu/cse486/sec1/quiz2/MainActivity.java
+++ Assignments/Theory/Quiz-2/Bangla Dictionary/app/src/main/java/com/jubair/nsu/cse486/sec1/quiz2/MainActivity.java
@@ -1,14 +1,46 @@
 package com.jubair.nsu.cse486.sec1.quiz2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.jubair.nsu.cse486.sec1.midterm.MESSAGE";
+    public static final String EXTRA_ITEMS = "com.jubair.nsu.cse486.sec1.midterm.ITEMS";
+    EditText englishWord;
+    EditText banglaWord;
+
+    // LinkedList to hold the data
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        englishWord = findViewById(R.id.editText_english);
+        banglaWord = findViewById(R.id.editText_bangla);
+    }
+
+
+    public void Save(View v){
+        String english = englishWord.getText().toString();
+        String bangla = banglaWord.getText().toString();
+
+        Intent i = new Intent(this, WordListAdapter.class);
+        i.putExtra(EXTRA_MESSAGE, english);
+        i.putExtra(EXTRA_ITEMS, bangla);
+
+    }
+
+    public void Dictionary(View v){
+        Intent i = new Intent(this, WordListAdapter.class);
+        startActivity(i);
     }
 }
