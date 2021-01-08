92509ddb8ba7f7bbbbd6e7c3a968ed20f2ffe5ee
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2021-01-08 13:41:27 +0600

    App Design #47 : Customer's Monthly Plan

--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Customer/home.java
+++
@@ -1,4 +0,0 @@
-package com.jubair.nsu.cse486.sec1.foodies.Customer;
-
-public class home {
-}
--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/MainActivity.java
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_explore_items);
+        setContentView(R.layout.activity_customer_monthly_plan);
     }
 
     public void OpenChef(View v) {
