6555112b5c28429fff54649f5d17ed472f959da5
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2021-01-10 01:47:41 +0600

    App Design #49 :Customer's Event Page

---
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Customer/Customer_Event.java
@@ -0,0 +1,16 @@
+package com.jubair.nsu.cse486.sec1.foodies.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jubair.nsu.cse486.sec1.foodies.R;
+
+public class Customer_Event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_event);
+    }
+}
