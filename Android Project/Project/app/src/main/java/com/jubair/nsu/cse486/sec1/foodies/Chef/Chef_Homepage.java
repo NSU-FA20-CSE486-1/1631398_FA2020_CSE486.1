25b03ee597846228423d8ddc07961150a72a0d44
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2021-01-11 05:07:05 +0600

    App Design #43 : Chef's Menu bar

--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Chef/Chef_Homepage.java
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Chef/Chef_Homepage.java
@@ -1,19 +1,52 @@
 package com.jubair.nsu.cse486.sec1.foodies.Chef;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 import com.jubair.nsu.cse486.sec1.foodies.R;
 
 public class Chef_Homepage extends AppCompatActivity {
+
+    //Variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Menu menu;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_homepage);
+
+        /*---------------------Hooks------------------------*/
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        textView=findViewById(R.id.textView);
     }
+
+
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {super.onBackPressed();
+        }
+    }
+
+
 
 
 }
