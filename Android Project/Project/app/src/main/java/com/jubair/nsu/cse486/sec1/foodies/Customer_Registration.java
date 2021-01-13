5bf55fadaf0a1248fa2ac94f664d5be10b2246f7
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2021-01-13 21:13:49 +0600

    Database Connection #66 : Firebase database for Login and Registration of Customers

--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Customer_Registration.java
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Customer_Registration.java
@@ -3,15 +3,73 @@ package com.jubair.nsu.cse486.sec1.foodies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.jubair.nsu.cse486.sec1.foodies.Customer.Customer_Homepage;
+
 
 public class Customer_Registration extends AppCompatActivity {
+
+    EditText name,phoneNo, emailid, password;
+    Button btnSignUp;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_registration);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        name = findViewById(R.id.editTextTextPersonName5);
+        phoneNo = findViewById(R.id.editTextPhone6);
+        emailid = findViewById(R.id.editTextEmailCustomer);
+        password = findViewById(R.id.editTextPasswordCustomer);
+        btnSignUp = findViewById(R.id.btnCustomerConfirm);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailid.getText().toString();
+                String pwd = emailid.getText().toString();
+                if(email.isEmpty()){
+                    emailid.setError("Please enter phone number");
+                    emailid.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    password.setError("Please enter your password");
+                    password.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(Customer_Registration.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(Customer_Registration.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(Customer_Registration.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(Customer_Registration.this, Customer_Homepage.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(Customer_Registration.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
     }
 
 
--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Customer_login.java
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Customer_login.java
@@ -3,17 +3,92 @@ package com.jubair.nsu.cse486.sec1.foodies;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.jubair.nsu.cse486.sec1.foodies.Chef.Chef_Homepage;
 import com.jubair.nsu.cse486.sec1.foodies.Customer.Customer_Homepage;
 
 public class Customer_login extends AppCompatActivity {
+
+    EditText emailid, password;
+    Button btnSignIn;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailid = findViewById(R.id.editTextTextEmailAddressCustomer);
+        password = findViewById(R.id.editTextTextPassword3);
+        btnSignIn = findViewById(R.id.btnCustomerLogin);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(Customer_login.this,"You are logged in",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(Customer_login.this, Customer_Homepage.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(Customer_login.this,"Please Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailid.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty()){
+                    emailid.setError("Please enter Phone");
+                    emailid.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    password.setError("Please enter your password");
+                    password.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(Customer_login.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(Customer_login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(Customer_login.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Intent intToHome = new Intent(Customer_login.this,Customer_Homepage.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(Customer_login.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
     }
 
 
