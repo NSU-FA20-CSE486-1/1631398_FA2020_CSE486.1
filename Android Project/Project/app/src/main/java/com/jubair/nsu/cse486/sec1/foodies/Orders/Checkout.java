4738101752c26b94d85f5eec1ad2729f44002964
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2021-01-19 22:15:07 +0600

    Customers order Event plans #65
    Customer choose their plans for their event and place order in checkout page.

--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Orders/Checkout.java
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Orders/Checkout.java
@@ -1,26 +1,44 @@
 package com.jubair.nsu.cse486.sec1.foodies.Orders;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.jubair.nsu.cse486.sec1.foodies.Chef.Chef_AddItem;
 import com.jubair.nsu.cse486.sec1.foodies.Customer.Customer_Event;
 import com.jubair.nsu.cse486.sec1.foodies.Customer.Customer_Homepage;
 import com.jubair.nsu.cse486.sec1.foodies.Customer.Customer_Items;
 import com.jubair.nsu.cse486.sec1.foodies.R;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class Checkout extends AppCompatActivity {
-    TextView setType;
-    TextView set_Price;
-    TextView showQuantity;
-    TextView showDate;
-    TextView showTotal;
-    TextView showPay;
- //   String price;
-    int price;
+    TextView setType,set_Price,showQuantity,showDate,showTotal,showPay;
+    EditText name,address,phone;
+    FirebaseAuth mFirebaseAuth;
+    FirebaseFirestore db;
+    ProgressDialog pd;
+    int price,total,pay;
+    String customerID, set,date,quantity;
 
 
     @Override
@@ -28,18 +46,25 @@ public class Checkout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        pd = new ProgressDialog(this);
+
         setType = findViewById(R.id.eventType);
         set_Price = findViewById(R.id.setPrice);
         showQuantity = findViewById(R.id.quantityText);
         showDate = findViewById(R.id.dateText);
         showTotal = findViewById(R.id.totalText);
         showPay = findViewById(R.id.payText);
+        name = findViewById(R.id.editTextTextPersonName7);
+        address = findViewById(R.id.editTextTextPersonName);
+        phone = findViewById(R.id.editTextTextPersonName4);
 
         Intent intent = getIntent();
 
-        String set = intent.getStringExtra(Customer_Event.EXTRA_SET);
-        String date = intent.getStringExtra(Customer_Event.EXTRA_DATE);
-        String quantity = intent.getStringExtra(Customer_Event.EXTRA_QUANTITY);
+         set = intent.getStringExtra(Customer_Event.EXTRA_SET);
+         date = intent.getStringExtra(Customer_Event.EXTRA_DATE);
+         quantity = intent.getStringExtra(Customer_Event.EXTRA_QUANTITY);
 
 
         //Price for each set  ------- NOT WORKING
@@ -61,8 +86,8 @@ public class Checkout extends AppCompatActivity {
         int Quantity = Integer.parseInt(quantity);  // Converting textview to integer
 
         //Calculate total amount
-        int total = p*Quantity ;
-        int pay = total+100;
+         total = p*Quantity ;
+         pay = total+100;
 
 
 
@@ -77,8 +102,70 @@ public class Checkout extends AppCompatActivity {
 
     public void placeOrder(View view){
 
+        String personName = name.getText().toString();
+        String personAddress = address.getText().toString();
+        String personPhone = phone.getText().toString();
 
-        startActivity(new Intent(this, ConfirmOrder.class));
+        if(personName.isEmpty()){
+            name.setError("Please enter name");
+            name.requestFocus();
+        }
+        else if(personAddress.isEmpty()){
+            address.setError("Please enter address");
+            address.requestFocus();
+        }
+        else if(personPhone.isEmpty()){
+            phone.setError("Please enter phone number");
+            phone.requestFocus();
+        }
+
+        else if(!(personName.isEmpty() && personAddress.isEmpty() && personPhone.isEmpty())){
+
+            //Set title of progress bar
+            pd.setTitle("Placing order for your event...");
+            //Show progress bar when user click confirm button
+            pd.show();
+
+            customerID = mFirebaseAuth.getCurrentUser().getEmail() ;
+    //        foodID = UUID.randomUUID().toString();
+
+
+
+            DocumentReference documentReference = db.collection("Orders").document("Event").collection(customerID).document()  ;
+
+            Map<String,Object> item = new HashMap<>();
+            item.put("Menu", set );
+            item.put("Quantity", quantity );
+            item.put("EventDate", date );
+            item.put("TotalAmount", pay );
+            item.put("CustomerName", personName);
+            item.put("CustomerAddress", personAddress);
+            item.put("CustomerPhone", personPhone);
+
+            //add data
+            documentReference.set(item).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    // this will be called when data is added successfully
+                    pd.dismiss();
+                    Toast.makeText(Checkout.this,"Posted Successfully",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Checkout.this, ConfirmOrder.class));
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // this will be called if there is error while uploading
+                    Toast.makeText(Checkout.this, e.getMessage() ,Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else{
+            Toast.makeText(Checkout.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
     }
 
 
