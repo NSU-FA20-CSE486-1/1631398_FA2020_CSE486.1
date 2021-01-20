15076dccd5f9ccc4a6cff70d04572e8fbbbea33e
Jubair-nsu1 <62503637+Jubair-nsu1@users.noreply.github.com>
2021-01-20 11:10:46 +0600

    Navigation Drawer #73
    Navigation Drawer for both Chefs and Customers

--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Chef/Chef_Homepage.java
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Chef/Chef_Homepage.java
@@ -1,6 +1,8 @@
 package com.jubair.nsu.cse486.sec1.foodies.Chef;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,28 +10,33 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.jubair.nsu.cse486.sec1.foodies.Adapter.TestAdapter;
+import com.jubair.nsu.cse486.sec1.foodies.LandingPage;
 import com.jubair.nsu.cse486.sec1.foodies.Model.modelTest;
 import com.jubair.nsu.cse486.sec1.foodies.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Chef_Homepage extends AppCompatActivity {
+public class Chef_Homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     RecyclerView recview;
     ArrayList<modelTest> datalist;
     FirebaseFirestore db;
     TestAdapter adapter;
+    FirebaseAuth mFirebaseAuth;
 
     //Variables
     DrawerLayout drawerLayout;
@@ -69,14 +76,15 @@ public class Chef_Homepage extends AppCompatActivity {
                 });
 
 
+        /*---------------------Navigation Drawer------------------------*/
+        drawerLayout=findViewById(R.id.drawer_layout_chef);
+        navigationView=findViewById(R.id.nav_view);
+
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
 
 
 
-
-        /*---------------------Hooks------------------------*/
-   //     drawerLayout=findViewById(R.id.drawer_layout_chef);
-   //     navigationView=findViewById(R.id.nav_view);
-   //     textView=findViewById(R.id.textView);
     }
 
 
@@ -94,6 +102,32 @@ public class Chef_Homepage extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent intent = new Intent(this, Chef_Homepage.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_account:
+                break;
+            case R.id.nav_profile:
+                Intent i = new Intent(this, Chef_Profile.class);
+                startActivity(i);
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_logout:
+                mFirebaseAuth.getInstance().signOut();
+                Toast.makeText(Chef_Homepage.this, "Logged Out Successfully",Toast.LENGTH_SHORT).show();
+                Intent i2 = new Intent(this, LandingPage.class);
+                startActivity(i2);
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 
 }
--- Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Customer/Customer_Homepage.java
+++ Android Project/Project/app/src/main/java/com/jubair/nsu/cse486/sec1/foodies/Customer/Customer_Homepage.java
@@ -1,23 +1,45 @@
 package com.jubair.nsu.cse486.sec1.foodies.Customer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.jubair.nsu.cse486.sec1.foodies.Chef.Chef_AddItem;
 import com.jubair.nsu.cse486.sec1.foodies.Chef.Chef_Homepage;
 import com.jubair.nsu.cse486.sec1.foodies.Chef.Chef_Kitchen;
+import com.jubair.nsu.cse486.sec1.foodies.Chef.Chef_Profile;
+import com.jubair.nsu.cse486.sec1.foodies.LandingPage;
 import com.jubair.nsu.cse486.sec1.foodies.R;
 
-public class Customer_Homepage extends AppCompatActivity {
+public class Customer_Homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_hompage);
+
+        /*---------------------Navigation Drawer------------------------*/
+        drawerLayout=findViewById(R.id.drawer_layout_chef);
+        navigationView=findViewById(R.id.nav_view);
+
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+
 
     public void exploreItems(View v) {
         startActivity(new Intent(this, Customer_Items.class));
@@ -36,9 +58,31 @@ public class Customer_Homepage extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent intent = new Intent(this, Chef_Homepage.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_account:
+                break;
+            case R.id.nav_profile:
+                Intent i = new Intent(this, Chef_Profile.class);
+                startActivity(i);
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_logout:
+                mFirebaseAuth.getInstance().signOut();
+                Toast.makeText(Customer_Homepage.this, "Logged Out Successfully",Toast.LENGTH_SHORT).show();
+                Intent i2 = new Intent(this, LandingPage.class);
+                startActivity(i2);
+                break;
+        }
 
-
-
-
-
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
+
